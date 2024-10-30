3b37b7f8f46f38ea98be44556f6e0a8d5c1f9020
Sylvador <sylvestervstolovoy@gmail.com>
2024-10-30 16:58:34 +0400

    fix: format

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,32 +1,29 @@
-import { Monitor } from './bybit-p2p-monitor';
 import { MappedRenderer } from './renderer';
 import { MakeRendereableProperty } from './types';
 import { makeRendereableProperty } from './utils';
 
-export function Renderable<T extends any>(options?: Pick<Parameters<MakeRendereableProperty>[1], 'include' | 'exclude' | 'renderKeyPrefix' | 'transformer'>) {
+export function Renderable<T extends any>(
+    options?: Pick<Parameters<MakeRendereableProperty>[1], 'include' | 'exclude' | 'renderKeyPrefix' | 'transformer'>,
+) {
     return function (
         _: undefined,
-        context: ClassFieldDecoratorContext<T, string | number | { [key: string]: string | number }>
+        context: ClassFieldDecoratorContext<T, string | number | { [key: string]: string | number }>,
     ) {
         const key = context.name.toString() as keyof T & string;
-        
+
         context.addInitializer(function () {
             const renderer = (this as any).renderer as MappedRenderer;
             if (!renderer) {
                 throw new Error('No renderer found in class');
             }
-            
-            makeRendereableProperty(
-                renderer,
-                {
-                    ...options as any,
-                    target: this,
-                    key,
-                },
-            );
-        })
-        
+
+            makeRendereableProperty(renderer, {
+                ...(options as any),
+                target: this,
+                key,
+            });
+        });
+
         return (initValue: any) => initValue;
-    }
-    
+    };
 }
